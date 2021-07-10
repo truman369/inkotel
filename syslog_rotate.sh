5fdc60554bb8cc8de70bfd3341ba3125c03e3668
Kirill Troitskiy <fds.gate@gmail.com>
2021-07-10 07:58:21 +0300

    Remove syslog rotate script (moving to ELK)

--- syslog_rotate.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# удаляем из базы данных все события старше 3 дней
-echo "delete from SystemEvents where datediff(now(), ReceivedAt) > 3; optimize table SystemEvents;" | mysql -u rsyslog -prsyslog Syslog 1>/dev/null
