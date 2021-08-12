54d34203220352b96ee3ba0a1f57f37ee64216e4
Kirill Troitskiy <fds.gate@gmail.com>
2021-08-12 16:22:21 +0300

    Add handling wrong value in full_ip

--- common.sh
+++ common.sh
@@ -17,11 +17,13 @@ function full_ip {
     ip=$1
     rx='([1-9]?[0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
     if [[ $ip =~ ^$rx\.$rx$ ]]; then
-        pre="192.168."
+        result="192.168.$ip"
+    elif [[ $ip =~ ^$rx\.$rx\.$rx\.$rx$ ]]; then
+        result="$ip"
     else
-        pre=""
+        result=""
     fi
-    echo "$pre$ip"
+    echo "$result"
 }
 
 # проверка, что значение содержится в списке
